aa4b40f129f490f63e2cc862891246d42bab5a80
briankipchirchir <kbrianngetich@gmail.com>
2023-03-14 22:49:43 +0300

    project

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance){
+    let result
+    if(distance<=400){
+        result='This one is on me!'
+    }
+    else if(distance>2000&&distance<2500){
+        result= 'I will gladly take your thirty bucks.'
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+    }
+    else if(distance>2500){
+        result='No can do.'
+    }
+    return result
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+}
+function ternaryCheckCity(city){
+    let ans
+    if(city=='NYC'){
+        ans='Ok, sounds good.'
+    }
+    else{
+        ans='No go.'
+    }
+    return ans
+}
+function switconcharafromtip(tip){
+    let ans
+    let tip
+    if(tip=='generous'){
+        ans="Thank you so much."
+    }
+    else if(tip=='not as generous'){
+        ans="Thank you."
+    }
+    else{
+        ans="Bye."
+    }
+    return ans
 }
